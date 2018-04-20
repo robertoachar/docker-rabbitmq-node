2149a5851497f25cf68c27c85a11c70854215297
Roberto Achar <robertoachar@gmail.com>
2018-04-20 17:28:01 -0300

    refactor(consumer): format using prettier

--- consumer/src/delay.js
+++ consumer/src/delay.js
@@ -1,1 +1,1 @@
-module.exports = d => new Promise(r => setTimeout(r, d));
+module.exports = (d) => new Promise((r) => setTimeout(r, d));
--- consumer/src/index.js
+++ consumer/src/index.js
@@ -2,7 +2,6 @@ const winston = require('winston');
 
 const broker = require('./broker');
 
-broker.start()
-  .catch((err) => {
-    winston.error(err);
-  });
+broker.start().catch((err) => {
+  winston.error(err);
+});
